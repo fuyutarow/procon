f3bcd925a2b8e429218a942ff5e5f0e8635b23f2
fuyutarow <fuyutarow@gmail.com>
2022-03-31 07:16:25 +0900

    Add multiset

--- alds-lib/src/collections/multiset.rs
+++ alds-lib/src/collections/multiset.rs
@@ -1,30 +1,90 @@
-use std::collections::BTreeMap;
+// https://github.com/tanakh/competitive-rs/blob/master/src/collections.rs
+
+use std::collections::{
+    btree_map::{Iter, Keys},
+    BTreeMap,
+};
 
 #[derive(Debug)]
-pub struct MultiSet<T>(BTreeMap<T, usize>);
+pub struct MultiSet<T>(pub BTreeMap<T, usize>);
 
 impl<T: Ord> MultiSet<T> {
     pub fn new() -> Self {
         Self(BTreeMap::new())
     }
 
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+
+    pub fn keys(&self) -> Keys<T, usize> {
+        self.0.keys()
+    }
+
+    pub fn get(&self, v: &T) -> Option<&usize> {
+        self.0.get(v)
+    }
+
+    pub fn get_mut(&mut self, v: &T) -> Option<&mut usize> {
+        self.0.get_mut(v)
+    }
+
+    pub fn contains(&self, v: &T) -> bool {
+        self.0.contains_key(v)
+    }
+
     pub fn insert(&mut self, v: T) {
-        *self.0.entry(v).or_default() += 1;
+        self.insert_times(v, 1);
+    }
+
+    pub fn insert_times(&mut self, v: T, n: usize) {
+        *self.0.entry(v).or_default() += n;
     }
 
     pub fn remove(&mut self, v: &T) {
+        self.remove_times(v, 1);
+    }
+
+    pub fn remove_times(&mut self, v: &T, n: usize) {
         let r = self.0.get_mut(v).unwrap();
-        *r -= 1;
+        *r = 0.min(*r - n);
         if *r == 0 {
             self.0.remove(v);
         }
     }
 
+    pub fn remove_all(&mut self, v: &T) {
+        let r = self.0.get_mut(v).unwrap();
+        self.0.remove(v);
+    }
+
     pub fn min(&self) -> Option<&T> {
-        self.0.iter().nth(0).map(|r| r.0)
+        self.0.iter().next().map(|r| r.0)
     }
 
     pub fn max(&self) -> Option<&T> {
         self.0.iter().last().map(|r| r.0)
+    }
+
+    pub fn iter(&self) -> Iter<T, usize> {
+        self.0.iter()
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.values().sum()
+    }
+
+    pub fn len_unique(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+impl<T: Ord> Default for MultiSet<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }
--- alds-lib/src/lib.rs
+++ alds-lib/src/lib.rs
@@ -1,1 +1,51 @@
 pub mod collections;
+
+#[macro_export]
+macro_rules! chmin {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {{
+        let cmp_min = _min!($($cmps),+);
+        if $base > cmp_min {
+            $base = cmp_min;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! chmax {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {{
+        let cmp_max = _max!($($cmps),+);
+        if $base < cmp_max {
+            $base = cmp_max;
+            true
+        } else {
+            false
+        }
+    }};
+}
+
+macro_rules! _min {
+    ($a:expr $(,)*) => {{
+        $a
+    }};
+    ($a:expr, $b:expr $(,)*) => {{
+        std::cmp::min($a, $b)
+    }};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{
+        std::cmp::min($a, min!($($rest),+))
+    }};
+}
+
+macro_rules! _max {
+    ($a:expr $(,)*) => {{
+        $a
+    }};
+    ($a:expr, $b:expr $(,)*) => {{
+        std::cmp::max($a, $b)
+    }};
+    ($a:expr, $($rest:expr),+ $(,)*) => {{
+        std::cmp::max($a, max!($($rest),+))
+    }};
+}
